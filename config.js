a9e8dc27580d96b8655a7061a2f4c3fd7cf4748f
SrivastavaRachit <rachitsrivastava789@gmail.com>
2022-02-12 11:31:52 +0530

    ..

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const api_config = {
-    db_url: 'mongodb+srv://mmm:mmm@cluster0.gvyon.mongodb.net/exam_miniproject?retryWrites=true&w=majority'
+    db_url: 'mongodb+srv://Rachit:rachit6518619@cluster0.yfa2c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }
 
 module.exports = api_config;
